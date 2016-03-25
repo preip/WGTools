78483a81299749cdd6fcfae5cfee53b65176234d
preip <p.reipschlaeger@googlemail.com>
2016-03-25 21:56:42 +0100

    fixed selected user and day number on cash page

--- controllers/cash.js
+++ controllers/cash.js
@@ -27,9 +27,9 @@ module.exports = function(dataPath) {
     function getCurrentDateString() {
         var date = new Date();
         var datestring = ''
-        if (date.getDay() < 10)
+        if (date.getDate() < 10)
             datestring += '0';
-        datestring += String(date.getDay()) + '.';
+        datestring += String(date.getDate()) + '.';
         if (date.getMonth() < 9)
             datestring += '0';
         datestring +=  String(date.getMonth() + 1) + '.' + String(date.getFullYear());
