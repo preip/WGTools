da162b6b3e8ca851856ff27ebbc3aac72fc539b3
Hannibal42 <hanischsimon@gmail.com>
2017-04-05 23:05:48 +0200

    Calendar with basic remove and add functionality

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,9 +1,62 @@
-    $(document).ready(function() {
+$(document).ready(function() {
 
-        // page is now ready, initialize the calendar...
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'today, prev, next',
+            center: 'title',
+            right: 'listMonth,month'
+        },
 
-        $('#calendar').fullCalendar({
-            // put your options and callbacks here
-        })
+        // customize the button names,
+        // otherwise they'd all just say "list"
+        views: {
+            listMonth: { buttonText: 'list month' }
+        },
+        aspectRatio: 1.5,
+        selectable: true,
+        eventLimit: true,
+        events: [
+            {
+                title: 'Test',
+                start: '2017-04-020',
+                allDay: true
+            },
+            {
+                title: 'Test',
+                start: '2017-04-019',
+                allDay: true
+            },
+            {
+                title: 'PutzenKatja',
+                start: '2017-04-019',
+                allDay: true
+            }
+        ],
+        
+        select: addEvent,
+        eventClick: removeEvent
+    })
+    
+    
+    function removeEvent(event, jsEvent, view) {
+        if(confirm("Remove event?")) {
+            $('#calendar').fullCalendar('removeEvents', event._id);
+        }
+    }
+    
+    function addEvent(start, end) {
+				var title = prompt('Event Title:');
+				var eventData;
+				if (title) {
+					eventData = {
+						title: title,
+						start: start,
+						end: end,
+                        allDay: true
+					};
+					$('#calendar').fullCalendar('renderEvent', eventData, true);
+				}
+				$('#calendar').fullCalendar('unselect');
+			}
 
-    });
+});
