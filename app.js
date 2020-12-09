def730c3c6c15871c88779ccc58b6d5c2a4a8708
Patrick Reipschläger <patrick.reipschlaeger@tu-dresden.de>
2020-12-09 20:40:27 +0100

    updated packages to current versions and renamed all views files, because jade is now named pug

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ try {
 global.app = express();
 app.set('port', process.env.PORT || config.port);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookies.express());
